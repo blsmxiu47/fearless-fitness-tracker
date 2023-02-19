f2f582001261625fbf61e50e8b8a1312f1932500
Wes Warriner <fridgehistory@gmail.com>
2023-02-19 11:12:43 -0700

    add import-data page

---
+++ pages/import-data.js
@@ -0,0 +1,24 @@
+import Head from 'next/head';
+import Link from "next/link";
+
+export default function ImportData() {
+    return (
+        <>
+            <Head>
+                <title>Import Data</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <h1>Import Data</h1>
+            <footer>
+                <Link href="/">Dashboard</Link>
+                <a href="#">My Stats</a>
+                <a href="#">Calendar</a>
+                <a href="#">Build Routine</a>
+                <a href="#">Explore</a>
+                <a href="#">Reports</a>
+                <a href="#">Preferences</a>
+                <Link href="#">Import Data</Link>
+            </footer>
+        </>
+    )
+}
