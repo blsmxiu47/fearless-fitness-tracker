3a8eb51223a35d7b7706929d2f0ac92806d94eba
Wes Warriner <fridgehistory@gmail.com>
2023-03-07 08:52:43 -0700

    add 'focus' model to db

--- prisma/seed/exercises.ts
+++ prisma/seed/exercises.ts
@@ -2,8 +2,9 @@ import { Prisma } from '@prisma/client'
 
 export const exerciseData: Prisma.ExerciseCreateInput[] = [
     {
-      name: 'VMO lifts',
-      type: 'Strength Training',
-      custom: false
+      name:   'VMO lifts',
+      type:   'Strength Training',
+      custom: false,
+      focus:  'Stability'
     }
   ]
---
+++ prisma/seed/focuses.ts
@@ -0,0 +1,8 @@
+import { Prisma } from '@prisma/client'
+
+export const focusData: Prisma.FocusCreateInput[] = [
+    {
+      name:   'Stability',
+      custom: false,
+    }
+  ]
