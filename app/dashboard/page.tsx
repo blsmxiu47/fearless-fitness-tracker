b9744552288f152dca1e5801feb680a6c8e3de6e
Wes Warriner <fridgehistory@gmail.com>
2023-12-16 11:10:38 -0700

    test adding filter component

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,6 +2,7 @@
 
 import '../../globals.css'
 import Card from '../components/Card';
+import Filter from '../components/Filter';
 import LinePlot from '../charts/LinePlot';
 import { useSidebar } from '../context/sidebar-provider'
 
@@ -9,14 +10,15 @@ export default function Home () {
     const { isSidebarOpen } = useSidebar();
     
     return (
-        <div className={`transition-all ${isSidebarOpen ? "sm:ml-64" : "sm:ml-16"}`}>
-            <div className="flex flex-wrap">
+        <div className={`py-2 transition-all ${isSidebarOpen ? "sm:ml-64" : "sm:ml-16"}`}>
+                <div className="flex flex-wrap justify-center p-2">
+                    <Filter />
+                </div>
                 <div className="flex flex-wrap my-4 px-1 w-full justify-center">
                     <Card title={"Activities"} content={<LinePlot data={[1,2,3,4]} />} />
                     <Card title={"Activities Time"} content={<LinePlot data={[1,2,3,4]} />} />
                     <Card title={"Distance"} content={<LinePlot data={[1,2,3,4]} />} />
                 </div>
-            </div>
         </div>
     )
 }
