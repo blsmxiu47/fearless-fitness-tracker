88db7ce2e89b217dcc55987c9465bb07ff1e6461
Wes Warriner <fridgehistory@gmail.com>
2023-12-20 19:51:50 -0700

    fix typo; adjust cards-per-row

--- app/components/ExploreCard.tsx
+++ app/components/ExploreCard.tsx
@@ -1,13 +1,13 @@
 import Link from 'next/link'
 
-export default function ExploredCard (fn: {
+export default function ExploreCard (fn: {
     title: string,
     subtitle: string,
     label: string,
     link: string
 }) {
     return (
-        <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 max-w-[240px]">
+        <div className="w-full md:w-1/2 lg:w-1/3 xl:w-1/4 max-w-[240px]">
             <Link href={fn.link}>
                 <div className="relative flex h-40 bg-white dark:bg-gray-800 justify-center p-2 m-2 overflow-hidden rounded-lg">
                     <div className="absolute top-0 right-0 bg-red-200 text-white text-xs px-2 py-1 rounded-bl-lg">
