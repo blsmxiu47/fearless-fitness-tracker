c5f1485695c3fdc2f746711fadb912473cebe501
Wes Warriner <fridgehistory@gmail.com>
2023-12-21 10:53:29 -0700

    mv plan route GET functionality to route.ts

---
+++ app/api/plan/route.ts
@@ -0,0 +1,26 @@
+import { NextRequest, NextResponse } from 'next/server'
+import prisma from '../../../lib/prisma'
+
+// GET /api/plan
+export async function GET(req: NextRequest) {
+    try {
+        const plans = await prisma.plans.findMany({
+            select: {
+                id: true,
+                name: true
+            }
+            // orderBy: [
+            //     {
+            //         id: 'asc',
+            //     },
+            // ],
+        })
+        return NextResponse.json(plans, { status: 200 })
+    } catch (error) {
+        console.error(error)
+        return NextResponse.json(
+            { error: 'An error occurred handling request to get plans.' },
+            { status: 500 }
+        )
+    }
+}
