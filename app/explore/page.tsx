b8b547ca2bf0f4f076e4c1a20ea891c19f589ae2
Wes Warriner <fridgehistory@gmail.com>
2023-12-20 19:52:39 -0700

    add dataless design for simple cards filter

--- app/explore/page.tsx
+++ app/explore/page.tsx
@@ -10,6 +10,25 @@ export default function Explore() {
     return (
         <div className={`py-2 transition-all ${isSidebarOpen ? "sm:ml-64" : "sm:ml-16"}`}>
             <h2>Explore</h2>
+            <div>
+                <span className="pr-2">Created by:</span>
+                <ul className="inline-flex gap-2">
+                    <li>
+                        <input checked type="checkbox" id="me-option" value="" className="hidden peer" />
+                        <label htmlFor="me-option" className="inline-flex justify-center px-4 p-1 cursor-pointer border-2 border-gray-200 rounded-full dark:hover:text-gray-300 dark:border-gray-700 peer-checked:border-red-200 hover:text-gray-600 dark:peer-checked:text-gray-300 peer-checked:text-gray-600 hover:bg-gray-50 dark:bg-gray-800 dark:hover:bg-gray-700">                           
+                            <span>Me</span>
+                        </label>
+                    </li>
+                    <li>
+                        <input checked type="checkbox" id="community-option" value="" className="hidden peer" />
+                        <label htmlFor="community-option" className="inline-flex justify-center px-4 py-1 cursor-pointer border-2 border-gray-200 rounded-full dark:hover:text-gray-300 dark:border-gray-700 peer-checked:border-red-200 hover:text-gray-600 dark:peer-checked:text-gray-300 peer-checked:text-gray-600 hover:bg-gray-50 dark:bg-gray-800 dark:hover:bg-gray-700">
+                            <span>Community</span>
+                        </label>
+                    </li>
+                </ul>
+
+
+            </div>
             <div className="mx-2 mt-4">
                 <h3>Exercises</h3>
                 <div className="flex flex-wrap justify-center sm:justify-start p-2">
