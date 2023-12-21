60e5d05d2c1404df291149be7fd32b620375ae1e
Wes Warriner <fridgehistory@gmail.com>
2023-12-21 09:36:59 -0700

    start bringing actual plan data from the fft api to rendered cards

--- app/explore/page.tsx
+++ app/explore/page.tsx
@@ -1,11 +1,47 @@
 'use client'
 
+import { useState, useEffect } from 'react'
+
 import '../../globals.css'
 import { useSidebar } from '../context/sidebar-provider'
 import ExploreCard from '../components/ExploreCard'
+import handle from '../api/plan'
+import { Plan } from '../../lib/types'
 
 export default function Explore() {
+    const [plans, setPlans] = useState<Plan[]>([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
     const { isSidebarOpen } = useSidebar();
+
+    const fetchPlans = async () => {
+        try {
+            const res = await fetch('/api/plan')
+            if (!res.ok) throw new Error(res.statusText)
+            return await res.json()
+        } catch (error) {
+            console.error('fetchPlans error:', error)
+            throw error;
+        }
+    }
+
+    // GET /api/plans to get all plans and put them in the plans array
+    useEffect(() => {
+        const loadPlans = async () => {
+            try {
+                const data = await fetchPlans();
+                setPlans(data);
+            } catch (err: any) {
+                setError(err.message);
+            } finally {
+                setIsLoading(false);
+            }
+        };
+
+        loadPlans();
+    }, []);
+    if (isLoading) return <div>Loading...</div>
+    if (error) return <div>{error}</div>
 
     return (
         <div className={`py-2 transition-all ${isSidebarOpen ? "sm:ml-64" : "sm:ml-16"}`}>
@@ -26,6 +62,18 @@ export default function Explore() {
                         </label>
                     </li>
                 </ul>
+            </div>
+            <div className="mx-2 mt-4">
+                <h3>Plans</h3>
+                <div className="flex flex-wrap justify-center sm:justify-start p-2">
+                    {plans.map((plan: Plan) => (
+                        <ExploreCard title={plan.name} subtitle='to be removed' label='to be removed' link={`/plan/${plan.id}`} />
+                    ))}
+                    <ExploreCard title={"Plan 1"} subtitle={"etw etw etw"} label={"Beginner"} link={"#"}/>
+                    <ExploreCard title={"Plan 1"} subtitle={"etw etw etw"} label={"Beginner"} link={"#"}/>
+                    <ExploreCard title={"Plan 1"} subtitle={"etw etw etw"} label={"Beginner"} link={"#"}/>
+                    <ExploreCard title={"Plan 1"} subtitle={"etw etw etw"} label={"Beginner"} link={"#"}/>
+                </div>
             </div>
             <div className="mx-2 mt-4">
                 <h3>Exercises</h3>
@@ -54,15 +102,6 @@ export default function Explore() {
                 <div className="flex flex-wrap justify-center sm:justify-start p-2">
                     <ExploreCard title={"Routine 1"} subtitle={"etw etw etw"} label={"Beginner"} link={"#"} />
                     <ExploreCard title={"Routine 1"} subtitle={"etw etw etw"} label={"Beginner"} link={"#"} />
-                </div>
-            </div>
-            <div className="mx-2 mt-4">
-                <h3>Plans</h3>
-                <div className="flex flex-wrap justify-center sm:justify-start p-2">
-                    <ExploreCard title={"Plan 1"} subtitle={"etw etw etw"} label={"Beginner"} link={"#"}/>
-                    <ExploreCard title={"Plan 1"} subtitle={"etw etw etw"} label={"Beginner"} link={"#"}/>
-                    <ExploreCard title={"Plan 1"} subtitle={"etw etw etw"} label={"Beginner"} link={"#"}/>
-                    <ExploreCard title={"Plan 1"} subtitle={"etw etw etw"} label={"Beginner"} link={"#"}/>
                 </div>
             </div>
         </div>
