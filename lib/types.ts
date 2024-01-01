5f1b50863a2b25d7e3ca56e932abf358b7f4fe04
Wes Warriner <fridgehistory@gmail.com>
2024-01-01 12:06:54 -0700

    remove (currently) unused types and add in ones that may actually be necessary for transforming exercises data

--- lib/types.ts
+++ lib/types.ts
@@ -1,58 +1,26 @@
-export interface User {
-    id: string;
-    email: string;
-    firstName: string;
-    password: string;
-    createdAt: Date;
-    updatedAt: Date;
+export interface Type {
+    id: number
+    user_id: number
+    name: string
 }
 
-export interface Plan {
-    id: string;
-    createdAt: Date;
-    updatedAt: Date;
-    userId: number;
-    name: string;
-    custom: boolean;
-    description?: string;
-    duration?: string;
-    frequency?: string;
-    tags?: string[];
+export interface Focus {
+    id: number
+    user_id: number
+    name: string
 }
 
-
-export interface Workout {
-    id: string;
-    exercises: string[];
-    datetime: Date;
-    userId: string;
-    title?: string | null;
-    notes?: string | null;
-    duration?: number | null;
-    distance?: number | null;
-    calories?: number | null;
-    averageHeartRate?: number | null;
-    maxHeartRate?: number | null;
-    averageRunCadence?: number | null;
-    maxRunCadence?: number | null;
-    averagePace?: number | null;
-    bestPace?: number | null;
-    averageLapTime?: number | null;
-    bestLapTime?: number | null;
-    numberOfLaps?: number | null;
-    totalAscent?: number | null;
-    totalDescent?: number | null;
-    averageStrideLength?: number | null;
-    minElevation?: number | null;
-    maxElevation?: number | null;
+export interface ExerciseTmp {
+    id: number
+    user_id: number
+    name: string
+    types: Type[]
+    focuses: Focus[]
 }
-
-export interface CustomEvent extends Event {
-    title: String,
-    workout: Workout,
-    start: Date,
-    end: Date,
-    allDay: boolean,
-    distance?: number,
-    duration?: number
+export interface Exercise {
+    id: number
+    user_id: number
+    name: string
+    type: Type
+    focus: Focus
 }
