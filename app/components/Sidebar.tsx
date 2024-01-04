68cbbcdae4a3a4777c4aa9f0e76b0b47641606cf
Wes Warriner <fridgehistory@gmail.com>
2024-01-03 20:03:48 -0700

    stage Preferences nav item for removal (consolidating with Account Settings)

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -113,12 +113,12 @@ export default function Sidebar() {
                             <span className={`inline-flex items-center ms-3 h-5 transition-all overflow-clip ${isSidebarOpen? "translate-x-0": "-translate-x-full opacity-0"}`}>Reports</span>
                         </a>
                     </li>
-                    <li>
+                    {/* <li>
                         <a href="/preferences" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
                             <LuWrench className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
                             <span className={`inline-flex items-center ms-3 h-5 transition-all overflow-clip ${isSidebarOpen? "-translate-x-0": "-translate-x-full opacity-0"}`}>Preferences</span>
                         </a>
-                    </li>
+                    </li> */}
                     <li>
                         <a href="/import-data" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
                             <LuImport className="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" />
