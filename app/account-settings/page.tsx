0b37d610e458f80248aba46a82c2e390a6274cea
Wes Warriner <fridgehistory@gmail.com>
2024-01-03 20:06:18 -0700

    begin populating settings page with two sections for small nav and settings edit form

--- app/account-settings/page.tsx
+++ app/account-settings/page.tsx
@@ -1,7 +1,20 @@
 import '../../globals.css'
+import SettingsNav from './SettingsNav'
 
 export default function AccountSettings() {
     return (
-        <h2>Account Settings</h2>
+        <div>
+            <h2>Settings</h2>
+            <hr />
+            <div className="grid grid-cols-1 sm:grid-cols-6 gap-4">
+                <div className="sm:col-start-1 sm:col-end-3">
+                    <SettingsNav />
+                </div>
+                
+                <div className="sm:col-start-4 sm:col-end-7">
+                    Form goes here
+                </div>
+            </div>
+        </div>
     )
 }
