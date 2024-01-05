49a9345141e1c39dd9aeb48ff12c7c19b3f0d884
Wes Warriner <fridgehistory@gmail.com>
2024-01-05 09:57:51 -0700

    update color palette; introduce new colors to build-routine page

--- app/build-routine/ExerciseInputForm.tsx
+++ app/build-routine/ExerciseInputForm.tsx
@@ -24,45 +24,45 @@ export default function ExerciseInputForm () {
             <h4 className="text-sm font-semibold">Add an Exercise</h4>
             <div className="flex items-center gap-2 my-2">
                 <label className="w-32" htmlFor="exercise-name">Exercise Name</label>
-                <input form="exercise-form" className="w-full pl-2 pr-6 py-2 overflow-hidden dark:bg-gray-800 rounded-md focus:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-[var(--accent-3)]" type="text" name="exercise-name" id="exercise-name" placeholder={"e.g. \"Plank\""} />
+                <input form="exercise-form" className="w-full pl-2 pr-6 py-2 overflow-hidden dark:bg-gray-800 rounded-md focus:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-[var(--accent-1)]" type="text" name="exercise-name" id="exercise-name" placeholder={"e.g. \"Plank\""} />
             </div>
             <div className="flex items-center gap-2 my-2">
                 <label className="w-32" htmlFor="exercise-sets">Sets</label>
-                <input form="exercise-form" className="w-full pl-2 pr-6 py-2 overflow-hidden dark:bg-gray-800 rounded-md focus:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-[var(--accent-3)]" type="number" name="exercise-sets" id="exercise-sets" min="1" max="99" placeholder="2" />
+                <input form="exercise-form" className="w-full pl-2 pr-6 py-2 overflow-hidden dark:bg-gray-800 rounded-md focus:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-[var(--accent-1)]" type="number" name="exercise-sets" id="exercise-sets" min="1" max="99" placeholder="2" />
             </div>
             <div className="flex items-center gap-2 my-2">
-                <select form="exercise-form" className="w-32 py-2 overflow-hidden dark:bg-gray-800 rounded-sm focus:bg-gray-700 focus:outline-none" name="exercise-measure-type" id="exercise-measure-type" value={measureType} onChange={e => handleMeasureTypeChange(e)}>
+                <select form="exercise-form" className="w-32 py-2 overflow-hidden dark:bg-gray-800 rounded-sm focus:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-[var(--accent-1)]" name="exercise-measure-type" id="exercise-measure-type" value={measureType} onChange={e => handleMeasureTypeChange(e)}>
                     {measureTypes.map((measureType, i) => (
                         <option key={i} value={measureType}>{measureType}</option>
                     ))}
                 </select>
                 {measureType === 'Reps' &&
-                    <input form="exercise-form" className="w-full pl-2 pr-6 py-2 overflow-hidden dark:bg-gray-800 rounded-md focus:bg-gray-700 focus:outline-none" type="text" name="exercise-reps" id="exercise-reps" placeholder={"e.g. \"8\" or \"10-12\""} />}
+                    <input form="exercise-form" className="w-full pl-2 pr-6 py-2 overflow-hidden dark:bg-gray-800 rounded-md focus:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-[var(--accent-1)]" type="text" name="exercise-reps" id="exercise-reps" placeholder={"e.g. \"8\" or \"10-12\""} />}
                 {measureType === 'Duration' &&
-                    <input form="exercise-form" className="w-full pl-2 pr-6 py-2 overflow-hidden dark:bg-gray-800 rounded-md focus:bg-gray-700 focus:outline-none" type="text" name="exercise-duration" id="exercise-duration" placeholder={"e.g. \"30 seconds\""} />}
+                    <input form="exercise-form" className="w-full pl-2 pr-6 py-2 overflow-hidden dark:bg-gray-800 rounded-md focus:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-[var(--accent-1)]" type="text" name="exercise-duration" id="exercise-duration" placeholder={"e.g. \"30 seconds\""} />}
             </div>
             <div className="flex items-center gap-2 my-2">
                 <label className="w-32" htmlFor="exercise-weight">Weight</label>
-                <input form="exercise-form" className="w-full pl-2 pr-6 py-2 overflow-hidden dark:bg-gray-800 rounded-md focus:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-[var(--accent-3)]" type="text" name="exercise-weight" id="exercise-weight" placeholder={"e.g. \"2 lbs\""} />
+                <input form="exercise-form" className="w-full pl-2 pr-6 py-2 overflow-hidden dark:bg-gray-800 rounded-md focus:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-[var(--accent-1)]" type="text" name="exercise-weight" id="exercise-weight" placeholder={"e.g. \"2 lbs\""} />
             </div>
             <div className="flex items-center gap-2 my-2">
                 <label className="w-32" htmlFor="exercise-rest">Rest</label>
-                <input form="exercise-form" className="w-full pl-2 pr-6 py-2 overflow-hidden dark:bg-gray-800 rounded-md focus:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-[var(--accent-3)]" type="text" name="exercise-rest" id="exercise-rest" placeholder={"e.g. \"30 seconds\""} />
+                <input form="exercise-form" className="w-full pl-2 pr-6 py-2 overflow-hidden dark:bg-gray-800 rounded-md focus:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-[var(--accent-1)]" type="text" name="exercise-rest" id="exercise-rest" placeholder={"e.g. \"30 seconds\""} />
             </div>
             <div className="flex items-center gap-2 my-2">
                 <label className="w-32" htmlFor="exercise-type">Type</label>
-                <input form="exercise-form" className="w-full pl-2 pr-6 py-2 overflow-hidden dark:bg-gray-800 rounded-md focus:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-[var(--accent-3)]" type="text" name="exercise-type" id="exercise-type" placeholder={"e.g. \"Core\""} />
+                <input form="exercise-form" className="w-full pl-2 pr-6 py-2 overflow-hidden dark:bg-gray-800 rounded-md focus:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-[var(--accent-1)]" type="text" name="exercise-type" id="exercise-type" placeholder={"e.g. \"Core\""} />
             </div>
             <div className="flex items-center gap-2 my-2">
                 <label className="w-32" htmlFor="exercise-focus">Focus</label>
-                <input form="exercise-form" className="w-full pl-2 pr-6 py-2 overflow-hidden dark:bg-gray-800 rounded-md focus:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-[var(--accent-3)]" type="text" name="exercise-focus" id="exercise-focus" placeholder={"e.g. \"Stability\""} />
+                <input form="exercise-form" className="w-full pl-2 pr-6 py-2 overflow-hidden dark:bg-gray-800 rounded-md focus:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-[var(--accent-1)]" type="text" name="exercise-focus" id="exercise-focus" placeholder={"e.g. \"Stability\""} />
             </div>
             <div className="flex items-center gap-2 my-2">
                 <label className="w-32" htmlFor="exercise-notes">Notes</label>
-                <textarea form="exercise-form" className="w-full pl-2 pr-6 py-2 overflow-hidden dark:bg-gray-800 rounded-md focus:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-[var(--accent-3)]" name="exercise-notes" id="exercise-notes" placeholder={"e.g. \"Remove the weight after the first two sets\""} />
+                <textarea form="exercise-form" className="w-full pl-2 pr-6 py-2 overflow-hidden dark:bg-gray-800 rounded-md focus:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-[var(--accent-1)]" name="exercise-notes" id="exercise-notes" placeholder={"e.g. \"Remove the weight after the first two sets\""} />
             </div>
             <div className="flex justify-center my-2">
-                <button form="exercise-form" type="submit" className="px-4 py-2 my-2 text-sm font-medium text-white bg-[var(--accent-1)] rounded-md hover:bg-[var(--accent-2)] focus:outline-none focus:ring-2 focus:ring-[var(--accent-3)]">
+                <button form="exercise-form" type="submit" className="px-4 py-2 my-2 text-sm font-medium text-white bg-[var(--primary)] rounded-md hover:bg-[var(--primary-dark)] focus:outline-none focus:ring-2 focus:ring-[var(--accent-1)]">
                     Save
                 </button>
             </div>
--- app/build-routine/page.tsx
+++ app/build-routine/page.tsx
@@ -22,24 +22,24 @@ export default function BuildRoutine() {
                     <div className="flex flex-col mx-2 max-w-[768px] text-xs sm:text-sm md:text-md">
                         <div className="flex items-center gap-2 my-2">
                             <label className="w-36" htmlFor="routine-name">Routine Name</label>
-                            <input form="routine-form" className="w-full pl-2 pr-6 py-2 overflow-hidden dark:bg-gray-800 rounded-md focus:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-[var(--accent-3)]" type="text" name="routine-name" id="routine-name" placeholder={"e.g. \"Core A\""} />
+                            <input form="routine-form" className="w-full pl-2 pr-6 py-2 overflow-hidden dark:bg-gray-800 rounded-md focus:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-[var(--accent-1)]" type="text" name="routine-name" id="routine-name" placeholder={"e.g. \"Core A\""} />
                         </div>
                         <div className="flex items-center gap-2 my-2">
                             <label className="w-36" htmlFor="routine-duration">Duration</label>
-                            <input form="routine-form" className="w-full pl-2 pr-6 py-2 overflow-hidden dark:bg-gray-800 rounded-md focus:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-[var(--accent-3)]" type="text" name="routine-duration" id="routine-duration" placeholder={"e.g. \"30 minutes\""} />
+                            <input form="routine-form" className="w-full pl-2 pr-6 py-2 overflow-hidden dark:bg-gray-800 rounded-md focus:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-[var(--accent-1)]" type="text" name="routine-duration" id="routine-duration" placeholder={"e.g. \"30 minutes\""} />
                         </div>
                         <div className="flex items-center gap-2 my-2">
                             <label className="w-36" htmlFor="routine-frequency">Frequency</label>
-                            <input form="routine-form" className="w-full pl-2 pr-6 py-2 overflow-hidden dark:bg-gray-800 rounded-md focus:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-[var(--accent-3)]" type="text" name="routine-frequency" id="routine-frequency" placeholder={"e.g. \"2 sessions per week\""} />
+                            <input form="routine-form" className="w-full pl-2 pr-6 py-2 overflow-hidden dark:bg-gray-800 rounded-md focus:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-[var(--accent-1)]" type="text" name="routine-frequency" id="routine-frequency" placeholder={"e.g. \"2 sessions per week\""} />
                         </div>
                         <div className="flex items-center gap-2 my-2">
                             <label className="w-36" htmlFor="routine-focus">Focus</label>
-                            <input form="routine-form" className="w-full pl-2 pr-6 py-2 overflow-hidden dark:bg-gray-800 rounded-md focus:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-[var(--accent-3)]" type="text" name="routine-focus" id="routine-focus" placeholder={"e.g. \"Stability\""} />
+                            <input form="routine-form" className="w-full pl-2 pr-6 py-2 overflow-hidden dark:bg-gray-800 rounded-md focus:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-[var(--accent-1)]" type="text" name="routine-focus" id="routine-focus" placeholder={"e.g. \"Stability\""} />
                         </div>
                     </div>
                     <h3 className="my-2">Exercises</h3>
                     <div className="mx-2">
-                        <button className="px-4 py-2 my-2 text-sm font-medium text-white bg-[var(--accent-1)] rounded-md hover:bg-[var(--accent-2)] focus:outline-none focus:ring-2 focus:ring-[var(--accent-3)]" onClick={handleAddExerciseClick} disabled={isFormVisible}>
+                        <button className="px-4 py-2 my-2 text-sm font-medium text-white bg-[var(--primary)] rounded-md hover:bg-[var(--primary-dark)] focus:outline-none focus:ring-2 focus:ring-[var(--accent-1)] disabled:opacity-70 disabled:hover:bg-[var(--primary)]" onClick={handleAddExerciseClick} disabled={isFormVisible}>
                             + Add Exercise
                         </button>
                         {isFormVisible && <ExerciseInputForm />}
