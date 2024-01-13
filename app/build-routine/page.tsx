76fe066e1ec4dc5c00b8aa6ac021948d02fbd4f3
Wes Warriner <fridgehistory@gmail.com>
2024-01-13 10:39:36 -0700

    ad required flags to inputs; add button for saving routine and TODO for handling validation

--- app/build-routine/page.tsx
+++ app/build-routine/page.tsx
@@ -6,11 +6,15 @@ import ExerciseInputForm from './ExerciseInputForm'
 
 export default function BuildRoutine() {
     const [isFormVisible, setIsFormVisible] = React.useState(false);
+    const [isRoutineValid, setIsRoutineValid] = React.useState(false);
     
     const handleAddExerciseClick = (e: React.MouseEvent<HTMLButtonElement>) => {
         e.preventDefault();
         setIsFormVisible(true);
     }
+
+    // isRoutineValid is true when the routine name, duration, frequency, and focus are all valid, and at least one exercise has been added
+    // TODO
 
     return (
         <>
@@ -22,19 +26,19 @@ export default function BuildRoutine() {
                     <div className="flex flex-col mx-2 max-w-[768px] text-xs sm:text-sm md:text-md">
                         <div className="flex items-center gap-2 my-2">
                             <label className="w-36" htmlFor="routine-name">Routine Name</label>
-                            <input form="routine-form" className="w-full pl-2 pr-6 py-2 overflow-hidden dark:bg-gray-800 rounded-md focus:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-[var(--accent-1)]" type="text" name="routine-name" id="routine-name" placeholder={"e.g. \"Core A\""} />
+                            <input form="routine-form" className="w-full pl-2 pr-6 py-2 overflow-hidden dark:bg-gray-800 rounded-md focus:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-[var(--accent-1)]" type="text" name="routine-name" id="routine-name" placeholder={"e.g. \"Core A\""} required />
                         </div>
                         <div className="flex items-center gap-2 my-2">
                             <label className="w-36" htmlFor="routine-duration">Duration</label>
-                            <input form="routine-form" className="w-full pl-2 pr-6 py-2 overflow-hidden dark:bg-gray-800 rounded-md focus:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-[var(--accent-1)]" type="text" name="routine-duration" id="routine-duration" placeholder={"e.g. \"30 minutes\""} />
+                            <input form="routine-form" className="w-full pl-2 pr-6 py-2 overflow-hidden dark:bg-gray-800 rounded-md focus:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-[var(--accent-1)]" type="text" name="routine-duration" id="routine-duration" placeholder={"e.g. \"30 minutes\""} required />
                         </div>
                         <div className="flex items-center gap-2 my-2">
                             <label className="w-36" htmlFor="routine-frequency">Frequency</label>
-                            <input form="routine-form" className="w-full pl-2 pr-6 py-2 overflow-hidden dark:bg-gray-800 rounded-md focus:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-[var(--accent-1)]" type="text" name="routine-frequency" id="routine-frequency" placeholder={"e.g. \"2 sessions per week\""} />
+                            <input form="routine-form" className="w-full pl-2 pr-6 py-2 overflow-hidden dark:bg-gray-800 rounded-md focus:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-[var(--accent-1)]" type="text" name="routine-frequency" id="routine-frequency" placeholder={"e.g. \"2 sessions per week\""} required />
                         </div>
                         <div className="flex items-center gap-2 my-2">
                             <label className="w-36" htmlFor="routine-focus">Focus</label>
-                            <input form="routine-form" className="w-full pl-2 pr-6 py-2 overflow-hidden dark:bg-gray-800 rounded-md focus:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-[var(--accent-1)]" type="text" name="routine-focus" id="routine-focus" placeholder={"e.g. \"Stability\""} />
+                            <input form="routine-form" className="w-full pl-2 pr-6 py-2 overflow-hidden dark:bg-gray-800 rounded-md focus:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-[var(--accent-1)]" type="text" name="routine-focus" id="routine-focus" placeholder={"e.g. \"Stability\""} required />
                         </div>
                     </div>
                     <h3 className="my-2">Exercises</h3>
@@ -43,6 +47,11 @@ export default function BuildRoutine() {
                             + Add Exercise
                         </button>
                         {isFormVisible && <ExerciseInputForm />}
+                    </div>
+                    <div>
+                        <button form="routine-form" className="px-4 py-2 my-2 text-sm font-medium text-white bg-[var(--primary)] rounded-md hover:bg-[var(--primary-dark)] focus:outline-none focus:ring-2 focus:ring-[var(--accent-1)] disabled:opacity-70 disabled:hover:bg-[var(--primary)]" type="submit" disabled={isRoutineValid}>
+                            Save Routine
+                        </button>
                     </div>
                 </div>
             </div>
