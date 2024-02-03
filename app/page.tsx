bf768a037402cbad2834650c32bafe6ad459fb5c
Wes Warriner <fridgehistory@gmail.com>
2024-02-03 10:19:57 -0700

    remove placeholder divs from home; bring home page up to speed wrt sidebar context

--- app/page.tsx
+++ app/page.tsx
@@ -1,134 +1,28 @@
 'use client'
 
 import '../globals.css'
-import { useSidebar } from './context/sidebar-provider'
 
 export default function Home () {
-    const { isSidebarOpen } = useSidebar();
-    
     return (
-        <div className={`transition-all ${isSidebarOpen ? "ml-64" : "ml-16"}`}>
-            <div>
-                <div className="flex flex-wrap -mx-2 lg:-mx-4">
-                    <div className="my-4 px-1 w-full">
-                        <div className="overflow-hidden rounded-lg shadow-lg my-2 md:my-4 lg:my-8">
-                            <div className="grid items-center justify-items-center p-4 md:p-8">
-                                <h2 className="text-md">Activities</h2>
-                                <div className="box-content">pyo</div>
-                            </div>
-                        </div>
-                        <div className="overflow-hidden rounded-lg shadow-lg my-2 md:my-4 lg:my-8">
-                            <div className="grid items-center justify-items-center p-4 md:p-8">
-                                <h2 className="text-md">Activities Time</h2>
-                                <div className="box-content">pyo</div>
-                            </div>
-                        </div>
-                        <div className="overflow-hidden rounded-lg shadow-lg my-2 md:my-4 lg:my-8">
-                            <div className="grid items-center justify-items-center p-4 md:p-8">
-                                <h2 className="text-md">Distance</h2>
-                                <div className="box-content">pyo</div>
-                            </div>
+        <div>
+            <div className="flex flex-wrap px-2 lg:px-4">
+                <div className="px-1 py-4 w-full">
+                    <div className="overflow-hidden rounded-lg shadow-lg my-2 md:my-4 lg:my-8">
+                        <div className="grid items-center justify-items-center p-4 md:p-8">
+                            <h2 className="text-md">Activities</h2>
+                            <div className="box-content">pyo</div>
                         </div>
                     </div>
-                </div>
-                <div className="p-4">
-                    <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
-                        <div className="grid grid-cols-3 gap-4 mb-4">
-                            <div className="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
-                                <p className="text-2xl text-gray-400 dark:text-gray-500">
-                                <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 1v16M1 9h16"/>
-                                </svg>
-                                </p>
-                            </div>
-                            <div className="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
-                                <p className="text-2xl text-gray-400 dark:text-gray-500">
-                                <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 1v16M1 9h16"/>
-                                </svg>
-                                </p>
-                            </div>
-                            <div className="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
-                                <p className="text-2xl text-gray-400 dark:text-gray-500">
-                                <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 1v16M1 9h16"/>
-                                </svg>
-                                </p>
-                            </div>
+                    <div className="overflow-hidden rounded-lg shadow-lg my-2 md:my-4 lg:my-8">
+                        <div className="grid items-center justify-items-center p-4 md:p-8">
+                            <h2 className="text-md">Activities Time</h2>
+                            <div className="box-content">pyo</div>
                         </div>
-                        <div className="flex items-center justify-center h-48 mb-4 rounded bg-gray-50 dark:bg-gray-800">
-                            <p className="text-2xl text-gray-400 dark:text-gray-500">
-                                <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 1v16M1 9h16"/>
-                                </svg>
-                            </p>
-                        </div>
-                        <div className="grid grid-cols-2 gap-4 mb-4">
-                            <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
-                                <p className="text-2xl text-gray-400 dark:text-gray-500">
-                                <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 1v16M1 9h16"/>
-                                </svg>
-                                </p>
-                            </div>
-                            <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
-                                <p className="text-2xl text-gray-400 dark:text-gray-500">
-                                <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 1v16M1 9h16"/>
-                                </svg>
-                                </p>
-                            </div>
-                            <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
-                                <p className="text-2xl text-gray-400 dark:text-gray-500">
-                                <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 1v16M1 9h16"/>
-                                </svg>
-                                </p>
-                            </div>
-                            <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
-                                <p className="text-2xl text-gray-400 dark:text-gray-500">
-                                <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 1v16M1 9h16"/>
-                                </svg>
-                                </p>
-                            </div>
-                        </div>
-                        <div className="flex items-center justify-center h-48 mb-4 rounded bg-gray-50 dark:bg-gray-800">
-                            <p className="text-2xl text-gray-400 dark:text-gray-500">
-                                <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 1v16M1 9h16"/>
-                                </svg>
-                            </p>
-                        </div>
-                        <div className="grid grid-cols-2 gap-4">
-                            <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
-                                <p className="text-2xl text-gray-400 dark:text-gray-500">
-                                <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 1v16M1 9h16"/>
-                                </svg>
-                                </p>
-                            </div>
-                            <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
-                                <p className="text-2xl text-gray-400 dark:text-gray-500">
-                                <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 1v16M1 9h16"/>
-                                </svg>
-                                </p>
-                            </div>
-                            <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
-                                <p className="text-2xl text-gray-400 dark:text-gray-500">
-                                <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 1v16M1 9h16"/>
-                                </svg>
-                                </p>
-                            </div>
-                            <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
-                                <p className="text-2xl text-gray-400 dark:text-gray-500">
-                                <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 1v16M1 9h16"/>
-                                </svg>
-                                </p>
-                            </div>
+                    </div>
+                    <div className="overflow-hidden rounded-lg shadow-lg my-2 md:my-4 lg:my-8">
+                        <div className="grid items-center justify-items-center p-4 md:p-8">
+                            <h2 className="text-md">Distance</h2>
+                            <div className="box-content">pyo</div>
                         </div>
                     </div>
                 </div>
