f335b35771bd46dbea00983c339e73527d7a86c9
Wes Warriner <fridgehistory@gmail.com>
2024-02-03 10:18:06 -0700

    add runs table

--- app/my-workouts/page.tsx
+++ app/my-workouts/page.tsx
@@ -1,28 +1,28 @@
 import Link from 'next/link'
 import { LuImport } from 'react-icons/lu'
 
+import MyRunsTable from './MyRunsTable'
 import MyWorkoutsTable from './MyWorkoutsTable'
 import '../../globals.css'
 
-export default function MyWorkouts() {
-
+export default function MySessions() {
     return (
-        <div>
-            <div className="mx-2 my-4 flex flex-col sm:flex-row sm:flex-wrap justify-between sm:items-center">
-                <h2 className="py-2">My Workouts</h2>
+        <div className="">
+            <div className="px-2 py-4 flex flex-col sm:flex-row sm:flex-wrap justify-between sm:items-center">
+                <h2 className="py-2">My Sessions</h2>
                 <div className="flex flex-wrap sm:flex-row gap-2 text-xs">
                         <button className="px-4 py-2 font-medium text-white bg-[var(--primary)] rounded-md hover:bg-[var(--primary-dark)] focus:outline-none focus:ring-2 focus:ring-[var(--accent-1)]">
-                            + Add Workout
+                            + Add Session
                         </button>
                         <button className="px-4 py-2 font-medium text-white bg-[var(--primary)] rounded-md hover:bg-[var(--primary-dark)] focus:outline-none focus:ring-2 focus:ring-[var(--accent-1)]">
                             Export CSV
                         </button>
                         <Link href="/import-data" className="flex border rounded-md gap-2 px-4 py-2 font-medium">
-                            <LuImport className="w-4 h-4 inline" />Import Workouts
+                            <LuImport className="w-4 h-4 inline" />Import Sessions
                         </Link>
                 </div>
             </div>
-            <div className="my-8 flex flex-col sm:flex-row sm:flex-wrap justify-between text-xs">
+            <div className="py-8 flex flex-col sm:flex-row sm:flex-wrap justify-between text-xs">
                 <div className="flex flex-col sm:flex-row sm:flex-wrap gap-2">
                     <div>
                         Search bar
@@ -37,6 +37,7 @@ export default function MyWorkouts() {
             </div>
             <div>
                 {/* table of results from prisma, with pagination. Sorting control as part of table header row. */}
+                <MyRunsTable />
                 <MyWorkoutsTable />
             </div>
         </div>
