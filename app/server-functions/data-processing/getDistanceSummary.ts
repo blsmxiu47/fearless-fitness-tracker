fdb930a3ec7440baa6ec648af04bba09465e24ad
Wes Warriner <fridgehistory@gmail.com>
2024-02-04 01:20:41 -0700

    add server-side query for summing total distance run by date

---
+++ app/server-functions/data-processing/getDistanceSummary.ts
@@ -0,0 +1,18 @@
+'use server';
+
+import prisma from '../../../lib/prisma';
+
+import { TimeSeriesResult } from '../../../lib/types';
+
+export default async function getDistanceSummary() {
+    const result = await prisma.$queryRaw<TimeSeriesResult[]>`
+        SELECT
+            DATE(datetime) as date,
+            EXTRACT(YEAR FROM datetime)::int as year,
+            EXTRACT(MONTH FROM datetime)::int as month,
+            To_Char(datetime, 'Dy') as dayofweek,
+            SUM(distance)::int as value FROM runs GROUP BY datetime ORDER BY datetime
+    `;
+
+    return result;
+}
