69a35118cf80fcc88652c94b22408d68022e496b
Wes Warriner <fridgehistory@gmail.com>
2024-02-04 14:36:07 -0700

    begin styling distance time series

--- app/charts/BarTimeSeries.tsx
+++ app/charts/BarTimeSeries.tsx
@@ -24,8 +24,8 @@ const BarTimeSeries: React.FC<BarTimeSeriesProps> = ({
     height = 400,
     marginTop = 20,
     marginRight = 20,
-    marginBottom = 20,
-    marginLeft = 20
+    marginBottom = 40,
+    marginLeft = 40
 }) => {
     const x = d3.scaleTime(data.map(d => d.date), [marginLeft, width - marginRight])
         .domain([d3.min(data, d => d.date) || new Date(), d3.max(data, d => d.date) || new Date()])
@@ -42,36 +42,37 @@ const BarTimeSeries: React.FC<BarTimeSeriesProps> = ({
             y={y(d.value)}
             width={10}
             height={y(0) - y(d.value)}
-            fill="steelblue"
+            fill={`var(--primary)`}
         />
     ));
 
     // Create x-axis ticks
     const xAxisTicks = x.ticks().map((tickValue) => (
         <g key={tickValue.toString()} transform={`translate(${x(tickValue)}, 0)`}>
-            <line y2={height - marginBottom - marginTop} stroke="white" />
-            <text style={{ textAnchor: 'middle', fill: 'white' }} dy=".71em" y={height - marginBottom + 10}>
+            <line y2={10} stroke="white" />
+            <text style={{ textAnchor: 'middle', font: '9px Arial', fill: 'white' }} dy=".16em" y={25}>
                 {d3.timeFormat("%Y-%m-%d")(tickValue)}
             </text>
         </g>
     ));
 
     // Create y-axis ticks
-    const yAxisTicks = y.ticks().map(tickValue => (
+    const yAxisTicks = y.ticks().map((tickValue) => (
         <g key={tickValue} transform={`translate(0, ${y(tickValue)})`}>
-            <line x2={width - marginLeft - marginRight} stroke="white" />
-            <text style={{ textAnchor: 'end', fill: 'white' }} x={-10} dy=".32em">
+            <line x2={width - marginLeft - marginRight / 2} stroke="white" />
+            <text style={{ textAnchor: 'end', font: '11px Arial', fill: 'white' }} x={-10} dx=".16em">
                 {tickValue}
             </text>
         </g>
     ));
+    console.log('yAxisTicks', yAxisTicks);
     
     // Format axes
     const xAxis = d3.axisBottom(x).tickSizeOuter(0);
     const yAxis = d3.axisLeft(y);
 
     return (
-        <svg width={width} height={height} style={{ maxWidth: '100%', height: 'auto' }}>
+        <svg width={width} height={height} style={{ maxWidth: '100%', height: 'auto' }} preserveAspectRatio='xMinYMin meet'>
             <g>{bars}</g>
             <g transform={`translate(0, ${height - marginBottom})`}>{xAxisTicks}</g>
             <g transform={`translate(${marginLeft}, 0)`}>{yAxisTicks}</g>
