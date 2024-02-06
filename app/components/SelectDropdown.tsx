c53586b80b2a439e2c73b879627b1ce41b33bf85
Wes Warriner <fridgehistory@gmail.com>
2024-02-06 12:50:52 -0700

    improve size/spacing of dropdown

--- app/components/SelectDropdown.tsx
+++ app/components/SelectDropdown.tsx
@@ -36,11 +36,11 @@ const SelectDropdown: React.FC<SelectDropdownProps> = ({
 
     return (
         <div className="relative w-full">
-            <div className="flex flex-col gap-1">
+            <div className="flex flex-col gap-1 pb-1">
                 <span className="text-gray-700 dark:text-gray-300 mr-2 font-semibold">Time Range</span>
                 <button
                     onClick={() => setDropdownVisible(!dropdownVisible)}
-                    className="pl-4 pr-10 py-2 mr-2 text-left bg-white dark:bg-gray-800 border border-gray-400 dark:border-gray-700 text-gray-700 dark:text-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-gray-400 dark:focus:ring-gray-600 cursor-pointer appearance-none form-select"
+                    className="pl-4 pr-10 py-2 text-left bg-white dark:bg-gray-800 border border-gray-400 dark:border-gray-700 text-gray-700 dark:text-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-gray-400 dark:focus:ring-gray-600 cursor-pointer appearance-none form-select"
                 >
                     {selectedOption}
                 </button>
@@ -48,7 +48,7 @@ const SelectDropdown: React.FC<SelectDropdownProps> = ({
             {dropdownVisible && (
                 <div
                     ref={dropdownRef}
-                    className="absolute z-10 bg-white dark:bg-gray-800 border border-gray-400 dark:border-gray-700 text-gray-700 dark:text-gray-300 rounded-md shadow-sm px-4 py-2 mr-2 focus:outline-none focus:ring-2 focus:ring-gray-400 dark:focus:ring-gray-600 cursor-pointer appearance-none form-select"
+                    className="absolute w-full px-4 py-2 z-10 bg-white dark:bg-gray-800 border border-gray-400 dark:border-gray-700 text-gray-700 dark:text-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-gray-400 dark:focus:ring-gray-600 cursor-pointer appearance-none form-select"
                 >
                     {optionGroups.map(([group, options]) => (
                         <optgroup key={group}>
