56517db94c1771ba247be1557ae8ee4ee1d93504
Wes Warriner <fridgehistory@gmail.com>
2024-02-06 12:30:57 -0700

    move TimeRangeSelect to SelectDropDown and refactor into a general select dropdown component (instead of time range values specific)

---
+++ app/components/SelectDropdown.tsx
@@ -0,0 +1,68 @@
+'use client'
+
+import { useEffect, useRef, useState } from 'react'
+
+type SelectDropdownProps = {
+    defaultOption: string;
+    optionGroups: Array<[number, string[]]>;
+    onChange: (option: string) => void;
+}
+
+const SelectDropdown: React.FC<SelectDropdownProps> = ({
+    defaultOption,
+    optionGroups,
+    onChange
+}) => {
+    const [dropdownVisible, setDropdownVisible] = useState(false)
+    const [selectedOption, setSelectedOption] = useState(defaultOption)
+    const dropdownRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        const handleClickOutside = (e: MouseEvent) => {
+            if (dropdownVisible && dropdownRef.current && !dropdownRef.current?.contains(e.target as Node)) {
+                setDropdownVisible(false)
+            }
+        }
+
+        window.addEventListener("mousedown", handleClickOutside)
+        return () => window.removeEventListener("mousedown", handleClickOutside)
+    }, [])
+
+    const handleOptionClick = (option: string) => {
+        setSelectedOption(option)
+        onChange(option)
+        setDropdownVisible(false)
+    }
+
+    return (
+        <div className="relative w-full">
+            <div className="flex flex-col gap-1">
+                <span className="text-gray-700 dark:text-gray-300 mr-2 font-semibold">Time Range</span>
+                <button
+                    onClick={() => setDropdownVisible(!dropdownVisible)}
+                    className="pl-4 pr-10 py-2 mr-2 text-left bg-white dark:bg-gray-800 border border-gray-400 dark:border-gray-700 text-gray-700 dark:text-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-gray-400 dark:focus:ring-gray-600 cursor-pointer appearance-none form-select"
+                >
+                    {selectedOption}
+                </button>
+            </div>
+            {dropdownVisible && (
+                <div
+                    ref={dropdownRef}
+                    className="absolute z-10 bg-white dark:bg-gray-800 border border-gray-400 dark:border-gray-700 text-gray-700 dark:text-gray-300 rounded-md shadow-sm px-4 py-2 mr-2 focus:outline-none focus:ring-2 focus:ring-gray-400 dark:focus:ring-gray-600 cursor-pointer appearance-none form-select"
+                >
+                    {optionGroups.map(([group, options]) => (
+                        <optgroup key={group}>
+                            {options.map((option, i) => (
+                                <option key={i} value={option} onClick={() => handleOptionClick(option)}>
+                                    {option}
+                                </option>
+                            ))}
+                        </optgroup>
+                    ))}
+                </div>
+            )}
+        </div>
+    )
+}
+
+export default SelectDropdown;
