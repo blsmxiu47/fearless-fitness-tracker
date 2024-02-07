1b0b49fd4858878b8cfcf34789b1fcdcadb33b39
Wes Warriner <fridgehistory@gmail.com>
2024-02-07 12:54:29 -0700

    add label arg

--- app/components/SelectDropdown.tsx
+++ app/components/SelectDropdown.tsx
@@ -3,12 +3,14 @@
 import { useEffect, useRef, useState } from 'react'
 
 type SelectDropdownProps = {
+    label: string;
     defaultOption: string;
     optionGroups: Array<[number, string[]]>;
     onChange: (option: string) => void;
 }
 
 const SelectDropdown: React.FC<SelectDropdownProps> = ({
+    label,
     defaultOption,
     optionGroups,
     onChange
@@ -37,7 +39,7 @@ const SelectDropdown: React.FC<SelectDropdownProps> = ({
     return (
         <div className="relative w-full">
             <div className="flex flex-col gap-1 pb-1">
-                <span className="text-gray-700 dark:text-gray-300 mr-2 font-semibold">Time Range</span>
+                <span className="text-gray-700 dark:text-gray-300 mr-2 font-semibold">{label}</span>
                 <button
                     onClick={() => setDropdownVisible(!dropdownVisible)}
                     className="pl-4 pr-10 py-2 text-left bg-white dark:bg-gray-800 border border-gray-400 dark:border-gray-700 text-gray-700 dark:text-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-gray-400 dark:focus:ring-gray-600 cursor-pointer appearance-none form-select"
