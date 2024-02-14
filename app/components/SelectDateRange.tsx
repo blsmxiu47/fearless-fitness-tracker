f8965adc4950840065c9296bf791b3ae189e8327
Wes Warriner <fridgehistory@gmail.com>
2024-02-14 10:51:02 -0700

    set max pickable dates to current date

--- app/components/SelectDateRange.tsx
+++ app/components/SelectDateRange.tsx
@@ -18,11 +18,23 @@ const SelectDateRange: React.FC<SelectDateRangeProps> = ({
     return (
         <div className="relative w-full">
             <div className="relative flex items-center">
-                <DatePicker disabled={!customRange} className="w-full px-4 py-2 rounded bg-white dark:bg-gray-800 border border-gray-400 dark:border-gray-700 text-gray-700 dark:text-gray-300 disabled:dark:bg-gray-900 disabled:dark:text-gray-500 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-gray-400 dark:focus:ring-gray-600 cursor-pointer appearance-none form-select" selected={startDate} onChange={(date) => onChange(date!, endDate)} />
+                <DatePicker
+                    disabled={!customRange}
+                    className="w-full px-4 py-2 rounded bg-white dark:bg-gray-800 border border-gray-400 dark:border-gray-700 text-gray-700 dark:text-gray-300 disabled:dark:bg-gray-900 disabled:dark:text-gray-500 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-gray-400 dark:focus:ring-gray-600 cursor-pointer appearance-none form-select"
+                    selected={startDate}
+                    onChange={(date) => onChange(date!, endDate)}
+                    maxDate={new Date()}
+                />
             </div>
             <span className="mx-4 text-gray-500">to</span>
             <div className="relative">
-                <DatePicker disabled={!customRange} className="w-full px-4 py-2 rounded bg-white dark:bg-gray-800 border border-gray-400 dark:border-gray-700 text-gray-700 dark:text-gray-300 disabled:dark:bg-gray-900 disabled:dark:text-gray-500 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-gray-400 dark:focus:ring-gray-600 cursor-pointer appearance-none form-select" selected={endDate} onChange={(date) => onChange(startDate, date!)} />
+                <DatePicker
+                    disabled={!customRange}
+                    className="w-full px-4 py-2 rounded bg-white dark:bg-gray-800 border border-gray-400 dark:border-gray-700 text-gray-700 dark:text-gray-300 disabled:dark:bg-gray-900 disabled:dark:text-gray-500 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-gray-400 dark:focus:ring-gray-600 cursor-pointer appearance-none form-select"
+                    selected={endDate}
+                    onChange={(date) => onChange(startDate, date!)}
+                    maxDate={new Date()}
+                />
             </div>
         </div>
     )
